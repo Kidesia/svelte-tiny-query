eb3f806c8a2983e7c93e4540ee45a8d29bc045fb
Flo <florian.unternaehrer@kidesia.ch>
2025-06-12 13:29:50 +0200

    Add multiple-queries test

--- tests/createQuery.svelte.test.ts
+++ tests/createQuery.svelte.test.ts
@@ -3,6 +3,7 @@ import { render, waitFor } from '@testing-library/svelte/svelte5';
 
 import TestContainerNoParam from './TestContainerNoParam.svelte';
 import TestContainerWithParam from './TestContainerWithParam.svelte';
+import TestContainerMultipleWithParams from './TestContainerMultipleWithParams.svelte';
 
 describe('createQuery', () => {
 	test('No param', async () => {
@@ -168,8 +169,6 @@ describe('createQuery', () => {
 			expect(rendered.queryByText('Error: oopsie')).toBeInTheDocument();
 		});
 
-		console.log(states.value);
-
 		expect(states.value).toEqual([
 			// Initial state
 			{
@@ -217,7 +216,7 @@ describe('createQuery', () => {
 		const rendered = render(TestContainerWithParam, {
 			props: {
 				states,
-				key: ['basic-cache-example'],
+				key: ['reactive-param-example'],
 				loadingFn: async (param: { id: number }) => ({
 					success: true,
 					data: `id is ${param.id}`
@@ -304,7 +303,7 @@ describe('createQuery', () => {
 		const rendered = render(TestContainerWithParam, {
 			props: {
 				states,
-				key: ['basic-cache-example'],
+				key: ['staletime-example'],
 				loadingFn: async (param: { id: number }) => ({
 					success: true,
 					data: `id is ${param.id}`
@@ -406,6 +405,113 @@ describe('createQuery', () => {
 				error: undefined,
 				loading: false,
 				staleTimeStamp: expectedStaleTime + 4000
+			}
+		]);
+	});
+
+	test('Multiple same queries with staletime', async () => {
+		vi.useFakeTimers();
+		const mockDate = new Date(2025, 5, 11, 12, 0, 0);
+		vi.setSystemTime(mockDate);
+
+		const mockLoadingFn = vi.fn(async (param: { id: number }) => ({
+			success: true as const,
+			data: `id is ${param.id}`
+		}));
+
+		const states1 = $state({ value: [] });
+		const states2 = $state({ value: [] });
+		const rendered = render(TestContainerMultipleWithParams, {
+			props: {
+				states1,
+				states2,
+				key: ['multiple-queries-example'],
+				loadingFn: mockLoadingFn,
+				queryOptions: {
+					staleTime: 2000
+				}
+			}
+		});
+
+		await waitFor(() => {
+			expect(rendered.queryByText('Data 1: id is 1')).toBeInTheDocument();
+		});
+
+		// even though there are two queries, the function will only be called once
+		expect(mockLoadingFn).toHaveBeenCalledTimes(1);
+
+		vi.advanceTimersByTime(1000);
+		rendered.queryByText('Increment 1')?.click();
+
+		await waitFor(() => {
+			expect(rendered.queryByText('Data 1: id is 2')).toBeInTheDocument();
+		});
+
+		vi.advanceTimersByTime(1000);
+		rendered.queryByText('Increment 2')?.click();
+
+		await waitFor(() => {
+			expect(rendered.queryByText('Data 2: id is 2')).toBeInTheDocument();
+		});
+
+		// even though both queries switched to param 2, the function will only be called once more
+		expect(mockLoadingFn).toHaveBeenCalledTimes(2);
+
+		expect(states1.value).toEqual([
+			// Initial state
+			{
+				data: undefined,
+				error: undefined,
+				loading: false,
+				staleTimeStamp: undefined
+			},
+			{
+				data: undefined,
+				error: undefined,
+				loading: true,
+				staleTimeStamp: undefined
+			},
+			{
+				data: 'id is 1',
+				error: undefined,
+				loading: false,
+				staleTimeStamp: mockDate.getTime() + 2000
+			},
+			// incrementing to id 2
+			{
+				data: undefined,
+				error: undefined,
+				loading: true,
+				staleTimeStamp: undefined
+			},
+			{
+				data: 'id is 2',
+				error: undefined,
+				loading: false,
+				staleTimeStamp: mockDate.getTime() + 3000
+			}
+		]);
+
+		expect(states2.value).toEqual([
+			// Initial state (already loading from query1)
+			{
+				data: undefined,
+				error: undefined,
+				loading: true,
+				staleTimeStamp: undefined
+			},
+			{
+				data: 'id is 1',
+				error: undefined,
+				loading: false,
+				staleTimeStamp: mockDate.getTime() + 2000
+			},
+			// incrementing to id 2 (from cache)
+			{
+				data: 'id is 2',
+				error: undefined,
+				loading: false,
+				staleTimeStamp: mockDate.getTime() + 3000
 			}
 		]);
 	});
