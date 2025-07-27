217faa999e3a44b1d56a346ebd5fd20648a81438
Flo <florian.unternaehrer@kidesia.ch>
2025-07-27 17:39:38 +0200

    More sequential tests

--- tests/createSequentialQuery/noParams.svelte.test.ts
+++ tests/createSequentialQuery/noParams.svelte.test.ts
@@ -2,11 +2,9 @@ import { describe, expect, test, vi } from 'vitest';
 import { render, waitFor } from '@testing-library/svelte/svelte5';
 
 import NoParam from './NoParam.svelte';
-// import WithParam from './WithParam.svelte';
-// import MultipleWithParams from './MultipleWithParams.svelte';
 
 describe('Sequential Query - No Parameter', () => {
-	test('Loads data successfully', async () => {
+	test('Loads first page', async () => {
 		vi.useFakeTimers();
 		const mockDate = new Date(2025, 5, 11, 12, 0, 0);
 		vi.setSystemTime(mockDate);
@@ -15,7 +13,7 @@ describe('Sequential Query - No Parameter', () => {
 		const rendered = render(NoParam, {
 			props: {
 				states,
-				key: ['simple-successful-test'],
+				key: ['successful-test'],
 				loadingFn: async () => ({
 					success: true,
 					data: 'payload',
@@ -50,7 +48,7 @@ describe('Sequential Query - No Parameter', () => {
 		]);
 	});
 
-	test('Loads more data successfully', async () => {
+	test('Error in first load', async () => {
 		vi.useFakeTimers();
 		const mockDate = new Date(2025, 5, 11, 12, 0, 0);
 		vi.setSystemTime(mockDate);
@@ -59,7 +57,53 @@ describe('Sequential Query - No Parameter', () => {
 		const rendered = render(NoParam, {
 			props: {
 				states,
-				key: ['simple-load-more-test'],
+				key: ['error-test'],
+				loadingFn: async () => ({
+					success: false,
+					error: 'Error loading data',
+					data: undefined
+				})
+			}
+		});
+
+		await waitFor(() => {
+			expect(
+				rendered.queryByText('Error: Error loading data')
+			).toBeInTheDocument();
+		});
+
+		expect(states.value).toEqual([
+			// Initial state
+			{
+				data: undefined,
+				error: undefined,
+				loading: true,
+				hasMore: undefined,
+				loadedTimeStamp: undefined,
+				staleTimeStamp: undefined
+			},
+			// After error
+			{
+				data: undefined,
+				error: 'Error loading data',
+				loading: false,
+				hasMore: undefined,
+				loadedTimeStamp: undefined,
+				staleTimeStamp: undefined
+			}
+		]);
+	});
+
+	test('Loads more data', async () => {
+		vi.useFakeTimers();
+		const mockDate = new Date(2025, 5, 11, 12, 0, 0);
+		vi.setSystemTime(mockDate);
+
+		const states = $state({ value: [] });
+		const rendered = render(NoParam, {
+			props: {
+				states,
+				key: ['load-more-test'],
 				loadingFn: async (_, cursor = 0) => ({
 					success: true,
 					data: [cursor, cursor + 1, cursor + 2],
@@ -160,6 +204,309 @@ describe('Sequential Query - No Parameter', () => {
 				staleTimeStamp: mockDate.getTime() + 2000
 			}
 			// No additional state change after clicking "Load More" again
+		]);
+	});
+
+	test('Error in load more', async () => {
+		vi.useFakeTimers();
+		const mockDate = new Date(2025, 5, 11, 12, 0, 0);
+		vi.setSystemTime(mockDate);
+
+		const states = $state({ value: [] });
+		const rendered = render(NoParam, {
+			props: {
+				states,
+				key: ['load-more-error-test'],
+				loadingFn: async (_, cursor = 0) => {
+					if (cursor === 0) {
+						return {
+							success: true,
+							data: [cursor, cursor + 1, cursor + 2],
+							cursor: 10
+						};
+					} else {
+						return {
+							success: false,
+							error: 'Error loading more data'
+						};
+					}
+				}
+			}
+		});
+
+		await waitFor(() => {
+			expect(rendered.queryByText('Data: [[0,1,2]]')).toBeInTheDocument();
+			expect(rendered.queryByText('Has More: Yes')).toBeInTheDocument();
+		});
+
+		vi.advanceTimersByTime(1000);
+		rendered.queryByText('Load More')?.click();
+		await waitFor(() => {
+			expect(
+				rendered.queryByText('Error: Error loading more data')
+			).toBeInTheDocument();
+		});
+
+		expect(states.value).toEqual([
+			// Initial state
+			{
+				data: undefined,
+				error: undefined,
+				loading: true,
+				hasMore: undefined,
+				loadedTimeStamp: undefined,
+				staleTimeStamp: undefined
+			},
+			// After loading first page
+			{
+				data: [[0, 1, 2]],
+				error: undefined,
+				loading: false,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime(),
+				staleTimeStamp: mockDate.getTime()
+			},
+			// Loading more data
+			{
+				data: [[0, 1, 2]],
+				error: undefined,
+				loading: true,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime(),
+				staleTimeStamp: mockDate.getTime()
+			},
+			// After error in load more
+			{
+				data: [[0, 1, 2]],
+				error: 'Error loading more data',
+				loading: false,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime(),
+				staleTimeStamp: mockDate.getTime()
+			}
+			// No additional state change after clicking "Load More" again
+		]);
+	});
+
+	test('Loads more then reloads', async () => {
+		vi.useFakeTimers();
+		const mockDate = new Date(2025, 5, 11, 12, 0, 0);
+		vi.setSystemTime(mockDate);
+
+		const states = $state({ value: [] });
+		const rendered = render(NoParam, {
+			props: {
+				states,
+				key: ['load-more-reload-test'],
+				loadingFn: async (_, cursor = 0) => ({
+					success: true,
+					data: [cursor, cursor + 1, cursor + 2],
+					cursor: cursor < 20 ? cursor + 10 : undefined
+				})
+			}
+		});
+
+		await waitFor(() => {
+			expect(rendered.queryByText('Data: [[0,1,2]]')).toBeInTheDocument();
+			expect(rendered.queryByText('Has More: Yes')).toBeInTheDocument();
+		});
+
+		vi.advanceTimersByTime(1000);
+		rendered.queryByText('Load More')?.click();
+		await waitFor(() => {
+			expect(
+				rendered.queryByText('Data: [[0,1,2],[10,11,12]]')
+			).toBeInTheDocument();
+			expect(rendered.queryByText('Has More: Yes')).toBeInTheDocument();
+		});
+
+		vi.advanceTimersByTime(1000);
+		rendered.queryByText('Reload')?.click();
+		await waitFor(() => {
+			expect(rendered.queryByText('Data: [[0,1,2]]')).toBeInTheDocument();
+			expect(rendered.queryByText('Has More: Yes')).toBeInTheDocument();
+		});
+
+		expect(states.value).toEqual([
+			// Initial state
+			{
+				data: undefined,
+				error: undefined,
+				loading: true,
+				hasMore: undefined,
+				loadedTimeStamp: undefined,
+				staleTimeStamp: undefined
+			},
+			// After loading first page
+			{
+				data: [[0, 1, 2]],
+				error: undefined,
+				loading: false,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime(),
+				staleTimeStamp: mockDate.getTime()
+			},
+			// Loading more data
+			{
+				data: [[0, 1, 2]],
+				error: undefined,
+				loading: true,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime(),
+				staleTimeStamp: mockDate.getTime()
+			},
+			// After loading second page
+			{
+				data: [
+					[0, 1, 2],
+					[10, 11, 12]
+				],
+				error: undefined,
+				loading: false,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime() + 1000,
+				staleTimeStamp: mockDate.getTime() + 1000
+			},
+			// Reloading data (TODO: should this reset the data by default?)
+			{
+				data: [
+					[0, 1, 2],
+					[10, 11, 12]
+				],
+				error: undefined,
+				loading: true,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime() + 1000,
+				staleTimeStamp: mockDate.getTime() + 1000
+			},
+			// After reloading data
+			{
+				data: [[0, 1, 2]],
+				error: undefined,
+				loading: false,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime() + 2000,
+				staleTimeStamp: mockDate.getTime() + 2000
+			}
+		]);
+	});
+
+	test('Error in reload', async () => {
+		vi.useFakeTimers();
+		const mockDate = new Date(2025, 5, 11, 12, 0, 0);
+		vi.setSystemTime(mockDate);
+
+		let i = $state(0);
+		const states = $state({ value: [] });
+		const rendered = render(NoParam, {
+			props: {
+				states,
+				key: ['load-more-reload-error-test'],
+				loadingFn: async (_, cursor = 0) => {
+					if (i < 2) {
+						i = i + 1;
+						return {
+							success: true,
+							data: [cursor, cursor + 1, cursor + 2],
+							cursor: cursor + 10
+						};
+					} else {
+						console.log('error loading data');
+						return {
+							success: false,
+							error: 'Error reloading data'
+						};
+					}
+				}
+			}
+		});
+
+		await waitFor(() => {
+			expect(rendered.queryByText('Data: [[0,1,2]]')).toBeInTheDocument();
+			expect(rendered.queryByText('Has More: Yes')).toBeInTheDocument();
+		});
+
+		vi.advanceTimersByTime(1000);
+		rendered.queryByText('Load More')?.click();
+		await waitFor(() => {
+			expect(
+				rendered.queryByText('Data: [[0,1,2],[10,11,12]]')
+			).toBeInTheDocument();
+			expect(rendered.queryByText('Has More: Yes')).toBeInTheDocument();
+		});
+
+		vi.advanceTimersByTime(1000);
+		rendered.queryByText('Reload')?.click();
+		await waitFor(() => {
+			expect(
+				rendered.queryByText('Error: Error reloading data')
+			).toBeInTheDocument();
+		});
+
+		expect(states.value).toEqual([
+			// Initial state
+			{
+				data: undefined,
+				error: undefined,
+				loading: true,
+				hasMore: undefined,
+				loadedTimeStamp: undefined,
+				staleTimeStamp: undefined
+			},
+			// After loading first page
+			{
+				data: [[0, 1, 2]],
+				error: undefined,
+				loading: false,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime(),
+				staleTimeStamp: mockDate.getTime()
+			},
+			// Loading more data
+			{
+				data: [[0, 1, 2]],
+				error: undefined,
+				loading: true,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime(),
+				staleTimeStamp: mockDate.getTime()
+			},
+			// After loading second page
+			{
+				data: [
+					[0, 1, 2],
+					[10, 11, 12]
+				],
+				error: undefined,
+				loading: false,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime() + 1000,
+				staleTimeStamp: mockDate.getTime() + 1000
+			},
+			// Reloading data
+			{
+				data: [
+					[0, 1, 2],
+					[10, 11, 12]
+				],
+				error: undefined,
+				loading: true,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime() + 1000,
+				staleTimeStamp: mockDate.getTime() + 1000
+			},
+			// After error in reload
+			{
+				data: [
+					[0, 1, 2],
+					[10, 11, 12]
+				],
+				error: 'Error reloading data',
+				loading: false,
+				hasMore: true,
+				loadedTimeStamp: mockDate.getTime() + 1000,
+				staleTimeStamp: mockDate.getTime() + 1000
+			}
 		]);
 	});
 });
